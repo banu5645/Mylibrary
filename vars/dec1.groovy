683a1fbc713ac0362f318b666fcc6c4d9b87e755
banu5645 <vignesh87781142@gmail.com>
2026-08-26 21:29:45 +0530

    Fix indentation in gitdownload function

--- vars/dec1.groovy
+++ vars/dec1.groovy
@@ -1,2 +1,2 @@
-def gitdownload(repo)
-{ git branch: main', url: "https://github.com/banu5645/${repo}.git" }
+def gitdownload(repo){
+  git branch: 'main', url: "https://github.com/banu5645/${repo}.git" }
