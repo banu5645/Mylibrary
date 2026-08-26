a04a04c19f3b81c7e60d3291d173a4704dac7ffe
banu5645 <vignesh87781142@gmail.com>
2026-08-26 22:24:27 +0530

    Add delivery function for WAR deployment
    Add delivery function to deploy WAR files.

--- vars/dec1.groovy
+++ vars/dec1.groovy
@@ -4,3 +4,5 @@ def buildartifact(){
   sh'mvn package'}
 def deploy(url,contextpath){
   deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'f21de2d3-84ee-4d2a-9955-34945d3c43cc', path: '', url: url)], contextPath: contextpath, war: '**/*.war'}
+def delivary(url,contextpath){
+  deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'f21de2d3-84ee-4d2a-9955-34945d3c43cc', path: '', url: url )], contextPath: contextpath, war: '**/*.war}
