e5849e099dcc27ed8b1e090a834259a24ecc7d68
banu5645 <vignesh87781142@gmail.com>
2026-08-26 21:54:20 +0530

    Add deploy function for Tomcat deployment

--- vars/dec1.groovy
+++ vars/dec1.groovy
@@ -2,3 +2,5 @@ def gitdownload(repo){
   git branch: 'main', url: "https://github.com/banu5645/${repo}.git" }
 def buildartifact(){
   sh'mvn package'}
+def deploy(url,contextpath){
+  deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'f21de2d3-84ee-4d2a-9955-34945d3c43cc', path: '', url: url)], contextPath: contextpath, war: '**/*.war'
